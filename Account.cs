9b5596062bbb14e273a0bf43f1d132da2118fc06
MehediHasan <nayan_hasan20@yahoo.com>
2020-11-09 14:02:41 +0600

    lab task6

---
+++ Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace account
+{ 
+    class ACCOUNT
+    {
+        private String accName;
+
+        private String acid;
+
+        private int balance;
+
+        public ACCOUNT(String accName, String acid, int balance)
+        {
+            this.accName = accName;
+            this.acid = acid;
+            this.balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            this.balance = balance + amount;
+            Console.WriteLine(balance);
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (balance > amount)
+            {
+                this.balance = balance - amount;
+                Console.WriteLine(balance);
+            }
+
+        }
+    }
---
+++ student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_task_3
+{
+    class Student
+    {
+        private String name;
+
+        private String id;
+
+        private String department;
+
+        private float cgpa;
+
+        public Student(String Name, String ID, String Department, float cgpa)
+        {
+            name = Name;
+            id = ID;
+            department = Department;
+            this.cgpa = cgpa;
+        }
+
+        public void showinfo()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+            Console.WriteLine(department);
+            Console.WriteLine(cgpa);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Mr.mehedi", "18-36328-1", "Cs", 3.7f);
+            s1.showinfo();
+           
+        }
+    }
+}
